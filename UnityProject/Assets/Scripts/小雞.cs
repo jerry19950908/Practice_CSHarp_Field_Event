f083b9a8a1b47ad98a7774798e45c3352822961e
jerry19950908 <57181340+jerry19950908@users.noreply.github.com>
2019-11-13 20:03:47 +0800

    作業

---
+++ UnityProject/Assets/Scripts/小雞.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class 小雞 : MonoBehaviour
+{
+    [Header("移動速度"), Range(0.5f, 100f)]
+    public float speed = 20.5f;
+    [Header("跳躍高度"), Range(10, 2000)]
+    public int jump = 30;
+    [Header("對話內容")]
+    public string talk = "咕咕咕~~";
+    [Header("是否取得雞蛋")]
+    public bool egg;
+
+    private void Start()
+    {
+        print("遊戲開始~");
+    }
+
+    private void Update()
+    {
+        print("咕咕咕~~");
+    }
+}
